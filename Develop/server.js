506e1d7144c1db4e8530d4113f12a5c637fe1398
hongbowu <wuhongbo87@gmail.com>
2024-01-02 17:23:05 -0500

    add notes route

--- Develop/server.js
+++ Develop/server.js
@@ -1,11 +1,15 @@
 const express = require("express")
 const path = require("path")
+
 const app = express()
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
+);
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.listen(PORT, ()=> console.log(`APP listening on port ${PORT}`));
