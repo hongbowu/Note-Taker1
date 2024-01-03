8629e3af31f76edff110be55d325887996907d47
hongbowu <wuhongbo87@gmail.com>
2024-01-02 22:57:57 -0500

    fix the post function

--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const path = require("path")
-const dbFile = require('./db/db.json')
+const { readFromFile, readAndAppend } = require('./helper/fsUtils');
 
 const app = express()
 app.use(express.json());
@@ -8,11 +8,23 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
+
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
-
+app.get('/api/notes', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+});
+app.post('/api/notes', (req, res) => {
+  readAndAppend(req.body , './db/db.json').then(() => {
+    res.json({ message: 'Note saved successfully!' });
+  });
+});
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
 
 app.listen(PORT, ()=> console.log(`APP listening on port ${PORT}`));
