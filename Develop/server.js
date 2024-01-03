4fce26753fa07871bc141d737fe7279c9d6ff7b9
hongbowu <wuhongbo87@gmail.com>
2024-01-02 21:24:02 -0500

    add helper file for read and write files

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,18 @@
 const express = require("express")
 const path = require("path")
+const dbFile = require('./db/db.json')
 
 const app = express()
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 
 const PORT = process.env.PORT || 3001;
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
 );
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/notes.html'))
-);
+
 
 app.listen(PORT, ()=> console.log(`APP listening on port ${PORT}`));
